8979b1a774f6be3893d96e0256153491ca8e2328
itprom <generation>
2022-07-12 14:38:47 +0500

    Исправлен RLE

--- src/org/tonkushin/Main.java
+++ src/org/tonkushin/Main.java
@@ -1,6 +1,7 @@
 package org.tonkushin;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+// Пример строки с аргументами
 //encode /home/anton/test.txt /home/anton/test.rle
 //decode /home/anton/test.rle /home/anton/test_decoded.txt
 
@@ -16,35 +18,25 @@ public class Main {
     private static String DECODE = "decode";
 
     public static void main(String[] args) throws IOException {
-        simpleTest();
-
         if (args.length < 3) {
             printHelp();
             return;
         }
 
-        Path readPath = Paths.get(args[1]);
-        Path writePath = Paths.get(args[2]);
+        String mode = args[0];
+        String sourceFilename = args[1];
+        String destFileName = args[2];
 
-        if (args[0].equals(ENCODE)) {
-            String inputString = Files.readAllLines(readPath).get(0);
-            byte[] bytes = RunLengthEncoder.encodeToByteArray(inputString);
-
-            try (FileOutputStream stream = new FileOutputStream(args[2])) {
-                stream.write(bytes);
-            }
-
-            System.out.printf("Файл %s успешно сжат в файл %s \n", args[1], args[2]);
-        } else if (args[0].equals(DECODE)) {
-            byte[] bytes;
-            try (FileInputStream stream = new FileInputStream(args[1])) {
-                bytes = stream.readAllBytes();
-            }
-
-            String decodedString = RunLengthEncoder.decodeFromByteArray(bytes);
-            Files.writeString(writePath, decodedString);
-
-            System.out.printf("Файл %s успешно распакован в файл %s \n", args[1], args[2]);
+        if (ENCODE.equals(mode)) {
+            byte[] src = readFile(sourceFilename);
+            byte[] bytes = RunLengthEncoder.encode(src);
+            writeFile(destFileName, bytes);
+            System.out.printf("Файл %s (%d байт) успешно сжат в файл %s (%d байт) \n", sourceFilename, src.length, destFileName, bytes.length);
+        } else if (DECODE.equals(mode)) {
+            byte[] src = readFile(sourceFilename);
+            byte[] bytes = RunLengthEncoder.decode(src);
+            writeFile(destFileName, bytes);
+            System.out.printf("Файл %s (%d байт) успешно распакован в файл %s (%d байт) \n", sourceFilename, src.length, destFileName, bytes.length);
         } else {
             printHelp();
         }
@@ -55,16 +47,24 @@ public class Main {
         System.out.println("1: Режим сжатия (encode) / распаковки (decode)");
         System.out.println("2: Путь к исходному файлу");
         System.out.println("3: Путь к результирующему файлу");
-        System.out.println("Текст не должен содержать цифры и символов переноса строки");
     }
 
-    private static void simpleTest() {
-        String inputString = "abbcccddddeeeeeffffffggggggghhhhhhhhjjjjjjjjjkkkkkkkkkklllllllllll";
-        byte[] bytes = RunLengthEncoder.encodeToByteArray(inputString);
-        String decodedString = RunLengthEncoder.decodeFromByteArray(bytes);
-        System.out.printf("Исходная строка: %s (%d)\n", inputString, inputString.length());
-        System.out.printf("Массив после сжатия: %s (%d)\n", Arrays.toString(bytes), bytes.length);
-        System.out.printf("Строка после распаковки: %s (%d)\n", decodedString, decodedString.length());
-        System.out.println(inputString.equals(decodedString) ? "Строки равны" : "Строки НЕ равны");
+    private static byte[] readFile(String filename){
+        byte[] bytes = null;
+        try (FileInputStream stream = new FileInputStream(filename)) {
+            bytes = stream.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bytes;
+    }
+
+    private static void writeFile(String filename, byte[] bytes) {
+        try (FileOutputStream stream = new FileOutputStream(filename)) {
+            stream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/org/tonkushin/RunLengthEncoder.java
+++ src/org/tonkushin/RunLengthEncoder.java
@@ -1,60 +1,25 @@
 package org.tonkushin;
 
 public class RunLengthEncoder {
-    public static String encode(String inputString) {
-        StringBuilder sb = new StringBuilder();
+    public static byte[] encode(byte[] input) {
+        VectorArray<Byte> vectorArray = new VectorArray<>(input.length / 2);
 
-        for (int i = 0; i < inputString.length(); i++) {
-            int count = 1;
-
-            while (i < inputString.length() - 1 && inputString.charAt(i) == inputString.charAt(i + 1)) {
-                count++;
-                i++;
-            }
-
-            sb.append(inputString.charAt(i)).append(count);
-        }
-
-        return sb.toString();
-    }
-
-    public static String decode(String decodedString) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < decodedString.length(); i++) {
-            char c = decodedString.charAt(i);
-
-            StringBuilder numberSb = new StringBuilder();
-            while (i + 1 < decodedString.length() && isNumber(String.valueOf(decodedString.charAt(i + 1)))) {
-                numberSb.append(decodedString.charAt(i + 1));
-                i++;
-            }
-
-            sb.append(repeat(c, Integer.parseInt(numberSb.toString())));
-        }
-
-        return sb.toString();
-    }
-
-    public static byte[] encodeToByteArray(String inputString) {
-        VectorArray<Byte> vectorArray = new VectorArray<>(inputString.length() / 2);
-
-        for (int i = 0; i < inputString.length(); i++) {
+        for (int i=0; i<input.length; i++){
             short count = 1;
 
-            while (i < inputString.length() - 1 && inputString.charAt(i) == inputString.charAt(i + 1)) {
+            while (i < input.length-1 && input[i] == input[i+1]) {
                 count++;
                 i++;
 
                 if (count == 255) {
-                    vectorArray.add((byte) inputString.charAt(i));
+                    vectorArray.add(input[i]);
                     vectorArray.add((byte) count);
 
                     count = 0;
                 }
             }
 
-            vectorArray.add((byte) inputString.charAt(i));
+            vectorArray.add(input[i]);
             vectorArray.add((byte) count);
         }
 
@@ -66,29 +31,20 @@ public class RunLengthEncoder {
         return retVal;
     }
 
-    public static String decodeFromByteArray(byte[] array) {
-        StringBuilder sb = new StringBuilder();
+    public static byte[] decode(byte[] input) {
+        VectorArray<Byte> vectorArray = new VectorArray<>(input.length);
 
-        for (int i = 0; i < array.length; i += 2) {
-            for (int j = 0; j < array[i + 1]; j++) {
-                sb.append((char) array[i]);
+        for (int i = 0; i < input.length; i += 2) {
+            for (int j = 0; j < input[i + 1]; j++) {
+                vectorArray.add(input[i]);
             }
         }
 
-        return sb.toString();
-    }
-
-    private static boolean isNumber(String str) {
-        return str.matches("^\\d*$");
-    }
-
-    private static String repeat(char c, int n) {
-        StringBuilder sb = new StringBuilder(n);
-
-        for (int i = 0; i < n; i++) {
-            sb.append(c);
+        byte[] retVal = new byte[vectorArray.size()];
+        for (int i = 0; i < vectorArray.size(); i++) {
+            retVal[i] = vectorArray.get(i);
         }
 
-        return sb.toString();
+        return retVal;
     }
 }
